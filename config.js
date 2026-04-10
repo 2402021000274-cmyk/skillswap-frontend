db53c3d60039d5e91856bc30e4a423bec95034de
2402021000274-cmyk <2402021000274@silveroakuni.ac.in>
2026-04-10 16:09:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,8 @@
 // ==========================================
-// 🔑 API, GOOGLE SCRIPT & FIREBASE SETUP
+// 🔑 API & GOOGLE SCRIPT URL
 // ==========================================
 const API_BASE_URL = "https://skillswap-backend-yexk.onrender.com"; // Backend ka address (Cloud Live)
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzRuReO-rxWTnQEQLRaaYkqb05z2hSw833TjLIx5Dd9dY8AAWVofH1iFLPkjTpLO4o-/exec";
-// 🟢 FIREBASE CONFIGURATION
-const firebaseConfig = {
-  apiKey: "AIzaSyDUhSdw6XbhNi1oTe4cOPLA_Ma1Vmg0fe8", // Navin API key yithe taka
-  authDomain: "skillhub-app-7ca09.firebaseapp.com",
-  projectId: "skillhub-app-7ca09",
-  storageBucket: "skillhub-app-7ca09.firebasestorage.app",
-  messagingSenderId: "422746633837",
-  appId: "1:422746633837:web:8cfe18f8ac282ec836b164",
-  measurementId: "G-H14JBLT470"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-auth.useDeviceLanguage(); // Set SMS language
 
 let usersDB = JSON.parse(localStorage.getItem('skillSwapUsers')) || [];
 
